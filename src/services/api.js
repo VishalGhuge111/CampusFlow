39a584affc23abe8e183cfc9f7a8df2938e4cd94
VishalGhuge111 <vishalghugesit@gmail.com>
2026-01-28 02:31:38 +0530

    update project

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const API_BASE_URL =
   window.location.hostname === "localhost"
     ? "http://localhost:5000/api"
-    : `http://${window.location.hostname}:5000/api`;
+    : "https://campusflow-backend-9uxk.onrender.com/api";
 
 const API = axios.create({
   baseURL: API_BASE_URL,
